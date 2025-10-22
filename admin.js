0f29298107faaa572477dc785754102b7e7eb53a
leleth-dotcom <leticia.hortelan@usp.br>
2025-10-22 17:50:43 -0300

    Delete admin.js

--- admin.js
+++
@@ -1,9 +0,0 @@
-
-function verificarSenha() {
-  const senha = document.getElementById('senha').value;
-  if (senha === 'renata2025') {
-    document.getElementById('painel').style.display = 'block';
-  } else {
-    alert('Senha incorreta');
-  }
-}
