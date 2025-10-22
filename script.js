ff66627b90d8712b3cfab96175faad53561b1197
leleth-dotcom <leticia.hortelan@usp.br>
2025-10-22 18:40:21 -0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-
-function showTab(tabId) {
-  document.getElementById('home').style.display = tabId === 'home' ? 'block' : 'none';
-  document.getElementById('form').style.display = tabId === 'form' ? 'block' : 'none';
-}
